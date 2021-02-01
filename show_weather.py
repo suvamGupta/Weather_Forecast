cf149080a3baa944eb975b90e90077e4744ca035
yashrajyash <yashrajyash18@gmail.com>
2021-02-01 12:07:21 +0530

    updated show_weather.py

--- show_weather.py
+++ show_weather.py
@@ -4,39 +4,53 @@ import json
 from datetime import datetime
 
 api_key = os.environ['open_weather']
-city_name = input('\nEnter the city name : ').strip()
 
-# api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+def fetch_data(city_name):
 
-complete_api_link = 'https://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=' + api_key
+    api_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=' + api_key
+    content = requests.get(api_url)
+    return content.json()
 
-api_link = requests.get(complete_api_link)
-api_data = api_link.json()
+def print_data(api_data):
 
-if api_data['cod'] == '404':
-    print(api_data['message'].capitalize())
-    a = input()
-else:
-    weather_desc = api_data['weather'][0]['description']
-    temp_city = float(api_data['main']['temp']) - 273.15
-    humid = api_data['main']['humidity']
-    wind_spd = api_data['wind']['speed']
-    city_name = api_data['name']
-    city_id = api_data['id']
-    date_time = datetime.now().strftime("%d-%b-%Y | %I:%M:%S %p")
+    if api_data['cod'] == '404':
+        print(api_data['message'].capitalize())
+    else:
+        weather_desc = api_data['weather'][0]['description']
+        temp_city = float(api_data['main']['temp']) - 273.15
+        humid = api_data['main']['humidity']
+        wind_spd = api_data['wind']['speed']
+        city_name = api_data['name']
+        city_id = api_data['id']
+        date_time = datetime.now().strftime("%d-%b-%Y | %I:%M:%S %p")
 
-    print('\n------------------------------------------------------------------------------------')
-    print('Weather stats for -> {} | City-id : {} | [{}]'.format(city_name, city_id, date_time))
-    print('------------------------------------------------------------------------------------\n')
+        print('\n------------------------------------------------------------------------------------')
+        print('Weather stats for -> {} | City-id : {} | [{}]'.format(city_name, city_id, date_time))
+        print('------------------------------------------------------------------------------------\n')
 
-    print('Current Temperature   :  {:.2f} deg Celcius'.format(temp_city))
-    print('Weather Discription   :  {}'.format(weather_desc))
-    print('Wind Speed            :  {} kmph'.format(wind_spd))
-    print('Humidity              :  {} %\n'.format(humid))
-    b = input()
+        print('Current Temperature   :  {:.2f} deg Celcius'.format(temp_city))
+        print('Weather Discription   :  {}'.format(weather_desc))
+        print('Wind Speed            :  {} kmph'.format(wind_spd))
+        print('Humidity              :  {} %\n'.format(humid))
 
-# def main():
-#     pass
+def main():
 
-# if __name__ == '__main__':
-#     main()
+    city = input('\nEnter the name of the city : ').strip()
+    raw_data = fetch_data(city)
+    print_data(raw_data)
+
+if __name__ == '__main__':
+
+    i = 1
+    while True:
+        if i == 1:
+            main()
+            i = 0
+        print('\n------------------------------------------------------------------------------------')
+        print('Search another city?')
+        op = input("Enter 'Y' for yes and 'N' for exit : ").strip()[0]
+        print('------------------------------------------------------------------------------------\n')
+        if(op.upper() == 'Y'):
+            main()
+        else:
+            exit()
